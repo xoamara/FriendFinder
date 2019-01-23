1bb0a34fa8195d366b6a2a7de0ec1bdd99a0965d
Nixinerix <xoamara2@gmail.com>
2019-01-23 13:07:34 -0800

    Have best match logic working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -18,23 +18,21 @@ api.post('/api/friends', function (req, res) {
   console.log("New User Input " + JSON.stringify(newUser));
 
   let newUserRatings = req.body.ratings;
-
-  console.log("New User Array STRING: " + newUserRatings);
-  let newUserConversion = 
-
-  console.log("New User Array: " + parseInt(newUserRatings));
-  console.log("Friend array at index 0: " + friends[0].ratings);
-
   let diff = [];
 
   for (var i = 0; i < friends.length; i++) {
-
     total = Math.abs(parseInt(friends[i].ratings) - parseInt(newUserRatings[i]));
-
     diff.push(total);
-    console.log(total);
-    console.log(" Total Difference " + diff);
   }
+
+  // Returns the lowest value from the diff array
+  let bestMatch = Math.min.apply(Math, diff);
+
+  // Provides the index position of the best match
+  let bestMatchIndexPosition = diff.indexOf(bestMatch);
+
+  // Returns the best match from user array - will use variable value to return the match to the client side
+  console.log(friends[bestMatchIndexPosition]);
 
   friends.push(newUser);
   // console.log(friends);
